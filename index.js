a4615dc85916d8bbf9838e8063dc49fef274f252
Saikat <sachinsingha7899@gmail.com>
2023-12-01 01:04:30 +0600

    store users on database]

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://Medical-camp:6s12bLm0EQPaNfmx@cluster0.ggrwjrl.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggrwjrl.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,11 +31,12 @@ async function run() {
     const joinCampCollection = client
       .db("MedicalCamp")
       .collection("joinCampData");
+    const usersCollection = client.db("MedicalCamp").collection("users");
 
-    // app.get("/availableCamp", async (req, res) => {
-    //   const result = await campCollection.find().toArray();
-    //   res.send(result);
-    // });
+    app.get("/availableCamp", async (req, res) => {
+      const result = await campCollection.find().toArray();
+      res.send(result);
+    });
 
     app.get("/popularCamps", async (req, res) => {
       const result = await campCollection
@@ -75,6 +75,17 @@ async function run() {
       } catch (error) {
         console.log(error);
       }
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send({ message: "user already exists", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
